de932697da4c0146dd3e9bd7dc76b35ab65505f8
uj2xxn <youjin1724@naver.com>
2025-01-20 15:37:30 +0900

    글씨 수정

--- src/styles/questsLow/QuestE4LStyles.jsx
+++ src/styles/questsLow/QuestE4LStyles.jsx
@@ -121,7 +121,7 @@ export const Level1Content = styled.div`
   top: 40%;
   left: 37%;
   color: white;
-  font-weight: bold;
+ 
 `;
 
 export const Level2 = styled.div`
@@ -166,13 +166,13 @@ export const Level2Content = styled.div`
   top: 40%;
   left: 37%;
   color: white;
-  font-weight: bold;
+  
 `;
 
 export const Level3 = styled.div`
   position: absolute;
   z-index: 1;
-  top: 39%;
+  top: 38.5%;
   padding: 0;
   margin-top: 46%;
   cursor: pointer;
@@ -211,13 +211,13 @@ export const Level3Content = styled.div`
   top: 40%;
   left: 37%;
   color: white;
-  font-weight: bold;
+
 `;
 
 export const Level4 = styled.div`
   position: absolute;
   z-index: 1;
-  top: 38%;
+  top: 37.6%;
   padding: 0;
   margin-top: 80%;
   cursor: pointer;
@@ -256,5 +256,5 @@ export const Level4Content = styled.div`
   top: 40%;
   left: 37%;
   color: white;
-  font-weight: bold;
+  
 `;
